311eafc47ea183670cedee78010d31ab91440d87
Abner-Santiago-7 <abnerdominguez48@gmail.com>
2024-07-29 22:10:34 -0600

    Se agrega el Proyecto Final nw-2024

---
+++ Config/Config.php
@@ -0,0 +1,16 @@
+<?php
+const BASE_URL = "http://localhost/ProyectoFinalNW2024/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "123456";
+const DB = "tienda-online";
+const CHARSET = "charset=utf8";
+const TITLE = "TIENDA ONLINE";
+const MONEDA = "USD";
+const CLIENT_ID = "AerO45CaJeQUG14uF5TvelsdxyNegmDTBuAoUKLkFEVhJqRXBU0Fwc_qZ104cJg2tZjEctP1oshTV0Mm";
+
+const USER_SMTP = "abnerdominguez48@gmail.com";
+const PASS_SMTP = "yakolzfhxazvctvi";
+const PUERTO_SMTP = 465;
+const HOST_SMTP = "smtp.gmail.com";
+?>
